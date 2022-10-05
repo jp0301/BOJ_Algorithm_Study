8ccd35d7bb4b8a1423f17ae814735e6ca90a5b28
Onedoli <rjswm78@gmail.com>
2022-10-05 20:13:39 +0900

    Update 나머지.java

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,17 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String []args) {
-        Scanner sc = new Scanner(System.in);
-        int a, b, c;
-        a = sc.nextInt();
-        b = sc.nextInt();
-        c = sc.nextInt();
-    
-        System.out.println((a+b)%c);
-        System.out.println(((a%c)+(b%c))%c);
-        System.out.println((a*b)%c);
-        System.out.println(((a%c)*(b%c))%c);
-        sc.close();
-    }
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+				
+		
+		
+		System.out.println( (a+b)%c );
+		System.out.println( ((a%c) + (b%c))%c );
+		System.out.println( (a*b)%c );
+		System.out.println( ((a%c) * (b%c))%c  );
+		
+		
+	}
 }
